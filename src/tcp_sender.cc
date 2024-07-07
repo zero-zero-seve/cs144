cbafbdb8a9084bb11ed25ca0955ee3002f8d7cd5
deer <1033059910@qq.com>
2024-07-07 23:07:25 +0800

    have finished the basic frame.There are much details wait for me to fill.And something must learn by the tests

--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -1,42 +1,124 @@
 #include "tcp_sender.hh"
+
 #include "tcp_config.hh"
 
 using namespace std;
 
-uint64_t TCPSender::sequence_numbers_in_flight() const
-{
-  // Your code here.
-  return {};
+uint64_t TCPSender::sequence_numbers_in_flight() const {
+    auto it = flight_msg.begin();
+    uint64_t sum = 0;
+    for (it; it != flight_msg.end(); it++) {
+        sum += it->second.sequence_length();
+    }
+    return sum;
 }
 
-uint64_t TCPSender::consecutive_retransmissions() const
-{
-  // Your code here.
-  return {};
+uint64_t TCPSender::consecutive_retransmissions() const { return consec_retransmission_numbers; }
+
+void TCPSender::push(const TransmitFunction& transmit) {
+    if (!isSyn) {
+        // 第一次运行给对方传输syn报文
+        TCPSenderMessage msg{isn_, true, nullptr, input_.reader().is_finished(), input_.reader().has_error()};
+        next_usefulSeq += 1;
+        transmit(msg);
+        return;
+    }
+    // 如果窗口框住序列号空间还有未使用序列号
+    if (recent_ackSeq + windowSize > next_usefulSeq) {
+        uint64_t len = recent_ackSeq + windowSize - next_usefulSeq;
+        std::string out = "";
+        while (len > TCPConfig::MAX_PAYLOAD_SIZE && !isFin) {
+            read(input_.reader(), TCPConfig::MAX_PAYLOAD_SIZE, out);
+            TCPSenderMessage msg{Wrap32::wrap(next_usefulSeq, isn_), false, out,
+                                 input_.reader().is_finished(), input_.reader().has_error()};
+            flight_msg.push_back(std::pair<uint64_t, TCPSenderMessage>(next_usefulSeq, msg));
+            transmit(msg);
+            if (isStoped) {
+                isStoped = false;
+            }
+            len = len - out.length() - input_.reader().is_finished();
+            next_usefulSeq = next_usefulSeq + out.length() + input_.reader().is_finished();
+            if (input_.reader().is_finished()) {
+                isFin = true;
+            }
+        }
+        if (len > 0 && !isFin) {
+            read(input_.reader(), len, out);
+            len = len - out.length();
+            TCPSenderMessage msg{Wrap32::wrap(next_usefulSeq, isn_), false, out, false,
+                                 input_.reader().has_error()};
+            if (input_.reader().is_finished()) {
+                if (len >= 1) {
+                    msg.FIN = true;
+                    isFin = true;
+                }
+            }
+            flight_msg.push_back(std::pair<uint64_t, TCPSenderMessage>(next_usefulSeq, msg));
+            transmit(msg);
+            if (isStoped) {
+                isStoped = false;
+            }
+            next_usefulSeq = out.length() + msg.FIN;
+        }
+        return;
+    } else {
+        TCPSenderMessage msg = make_empty_message();
+        transmit(msg);
+        return;
+    }
 }
 
-void TCPSender::push( const TransmitFunction& transmit )
-{
-  // Your code here.
-  (void)transmit;
+TCPSenderMessage TCPSender::make_empty_message() const {
+    TCPSenderMessage msg{Wrap32::wrap(next_usefulSeq, isn_), false, nullptr, false,
+                         input_.reader().has_error()};
+    return msg;
 }
 
-TCPSenderMessage TCPSender::make_empty_message() const
-{
-  // Your code here.
-  return {};
+void TCPSender::receive(const TCPReceiverMessage& msg) {
+    if (msg.RST) {
+        input_.set_error();
+    } else if (msg.ackno != std::nullopt) {
+        // 把ack号转化为绝对序列号
+        uint64_t temp = msg.ackno.unwrap(isn_, recent_ackSeq);
+        // 尽可能确认，而后从队列中移除
+        // TODO: 第一个发出但是没有得到确认ack并不等于接受方返回ack
+        auto it = flight_msg.begin();
+        for (; it != flight_msg.end(); it++) {
+            if (it->first + it->second.sequence_length() <= temp) {
+                flight_msg.pop_front();
+                remaining_time = initial_RTO_ms_;
+                isStoped = true;
+                consec_retransmission_numbers = 0;
+                first_unacked_seq = it->first + it->second.sequence_length();
+            } else {
+                break;
+            }
+        }
+        if (it == flight_msg.end()) {
+            isStoped = false;
+        }
+        if (recent_ackSeq < temp) {
+            recent_ackSeq = temp;
+        }
+        windowSize = msg.window_size;
+    }
 }
 
-void TCPSender::receive( const TCPReceiverMessage& msg )
-{
-  // Your code here.
-  (void)msg;
-}
-
-void TCPSender::tick( uint64_t ms_since_last_tick, const TransmitFunction& transmit )
-{
-  // Your code here.
-  (void)ms_since_last_tick;
-  (void)transmit;
-  (void)initial_RTO_ms_;
+void TCPSender::tick(uint64_t ms_since_last_tick, const TransmitFunction& transmit) {
+    if (isStoped) {
+        return;
+    }
+    if (ms_since_last_tick >= remaining_time) {
+        remaining_time = 0;
+        // 超时进行重传
+        auto it = flight_msg.begin();
+        if (it != flight_msg.end()) {
+            transmit(it->second);
+        }
+        // TODO: 如果窗口大小非0，跟踪连续次数？？
+        consec_retransmission_numbers += 1;
+        remaining_time = (1UL << consec_retransmission_numbers) * initial_RTO_ms_;
+    } else {
+        remaining_time -= ms_since_last_tick;
+    }
 }
--- src/tcp_sender.hh
+++ src/tcp_sender.hh
@@ -1,51 +1,66 @@
 #pragma once
 
-#include "byte_stream.hh"
-#include "tcp_receiver_message.hh"
-#include "tcp_sender_message.hh"
-
 #include <cstdint>
+#include <deque>
 #include <functional>
 #include <list>
 #include <memory>
 #include <optional>
 #include <queue>
 
-class TCPSender
-{
-public:
-  /* Construct TCP sender with given default Retransmission Timeout and possible ISN */
-  TCPSender( ByteStream&& input, Wrap32 isn, uint64_t initial_RTO_ms )
-    : input_( std::move( input ) ), isn_( isn ), initial_RTO_ms_( initial_RTO_ms )
-  {}
+#include "byte_stream.hh"
+#include "tcp_receiver_message.hh"
+#include "tcp_sender_message.hh"
 
-  /* Generate an empty TCPSenderMessage */
-  TCPSenderMessage make_empty_message() const;
+class TCPSender {
+   public:
+    /* Construct TCP sender with given default Retransmission Timeout and possible ISN */
+    TCPSender(ByteStream&& input, Wrap32 isn, uint64_t initial_RTO_ms)
+        : input_(std::move(input)),
+          isn_(isn),
+          initial_RTO_ms_(initial_RTO_ms),
+          remaining_time(initial_RTO_ms) {}
 
-  /* Receive and process a TCPReceiverMessage from the peer's receiver */
-  void receive( const TCPReceiverMessage& msg );
+    /* Generate an empty TCPSenderMessage */
+    TCPSenderMessage make_empty_message() const;
 
-  /* Type of the `transmit` function that the push and tick methods can use to send messages */
-  using TransmitFunction = std::function<void( const TCPSenderMessage& )>;
+    /* Receive and process a TCPReceiverMessage from the peer's receiver */
+    void receive(const TCPReceiverMessage& msg);
 
-  /* Push bytes from the outbound stream */
-  void push( const TransmitFunction& transmit );
+    /* Type of the `transmit` function that the push and tick methods can use to send messages */
+    using TransmitFunction = std::function<void(const TCPSenderMessage&)>;
 
-  /* Time has passed by the given # of milliseconds since the last time the tick() method was called */
-  void tick( uint64_t ms_since_last_tick, const TransmitFunction& transmit );
+    /* Push bytes from the outbound stream */
+    void push(const TransmitFunction& transmit);
 
-  // Accessors
-  uint64_t sequence_numbers_in_flight() const;  // How many sequence numbers are outstanding?
-  uint64_t consecutive_retransmissions() const; // How many consecutive *re*transmissions have happened?
-  Writer& writer() { return input_.writer(); }
-  const Writer& writer() const { return input_.writer(); }
+    /* Time has passed by the given # of milliseconds since the last time the tick() method was called */
+    void tick(uint64_t ms_since_last_tick, const TransmitFunction& transmit);
 
-  // Access input stream reader, but const-only (can't read from outside)
-  const Reader& reader() const { return input_.reader(); }
+    // Accessors
+    uint64_t sequence_numbers_in_flight() const;   // How many sequence numbers are outstanding?
+    uint64_t consecutive_retransmissions() const;  // How many consecutive *re*transmissions have happened?
+    Writer& writer() { return input_.writer(); }
+    const Writer& writer() const { return input_.writer(); }
 
-private:
-  // Variables initialized in constructor
-  ByteStream input_;
-  Wrap32 isn_;
-  uint64_t initial_RTO_ms_;
+    // Access input stream reader, but const-only (can't read from outside)
+    const Reader& reader() const { return input_.reader(); }
+
+   private:
+    // Variables initialized in constructor
+    ByteStream input_;
+    Wrap32 isn_;
+    uint64_t initial_RTO_ms_;
+    // TODO: 维护窗口大小使用
+    uint64_t windowSize{1};
+    // TODO: 最近从接受方收到的ack号，即期待我发送下一个序列号
+    uint64_t recent_ackSeq{0};
+    // TODO: 下一个可用序列号
+    uint64_t next_usefulSeq{0};
+    bool isSyn{false};
+    uint64_t consec_retransmission_numbers{0};
+    std::deque<std::pair<uint64_t, TCPSenderMessage>> flight_msg;
+    uint64_t remaining_time;
+    bool isStoped{true};
+    bool isFin{false};
+    uint64_t first_unacked_seq{0};
 };
